29326968442b196dd5ce5bbb708642b0381dd05f
Cody2612 <b.cody_26@yahoo.com>
2024-10-14 20:09:16 +0100

    remove the unnecessary console log

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -4,7 +4,6 @@ exports.getTopics = (request, response, next) => {
     fetchTopics()
     .then((topics)=>{
          response.status(200).send({topics});
-         console.log("this is from controller <<<<", {topics});
     })
     .catch(next);
 };
