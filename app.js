6b9c51d1cd9590e88f6cf753feaf32186802d7af
Cody2612 <b.cody_26@yahoo.com>
2024-10-15 15:47:27 +0100

    add articleId endpoint

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const app = express();
 const { getApis } = require("./controllers/api.controllers");
+const { getArticleById } = require("./controllers/article-id.controllers");
 
 app.get("/api", getApis);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById)
+
 app.all("*", (request, response, next) => {
   response.status(404).send({ msg: "End point not found" });
+});
+
+
+
+app.use((error, request, response, next) => {
+  if (error.code === "22P02"){
+    response.status(400).send({msg: "Invalid Id type"})
+  }
+  next(error);
+});
+
+app.use((error, request, response, next) => {
+  if (error.status && error.msg){
+    response.status(error.status).send({msg: error.msg})
+  }
+  next(error);
 });
 
 app.use((error, request, response, next) => {
