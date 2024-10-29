8c4ab3b08c325f797cf222187a97d7a173ba2739
Cody2612 <b.cody_26@yahoo.com>
2024-10-29 00:13:36 +0000

    deleted listener.js

--- listener.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-
-app.listen(8080, (err) => {
-    if (err) {
-        console.log("Server is down", err); 
-    } else {
-    console.log ("Server is listening on port:8080...");
-    }
-});
